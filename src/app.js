f62854d7943a13323f7519d2810c9d0f5d9df41d
PedroLucasSS <pedro.lucas4431@gmail.com>
2020-06-15 18:32:10 -0300

    finalizando rotas

--- src/app.js
+++ src/app.js
@@ -70,11 +70,11 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params
 
-  if(!isUuid(id)){
+  const repository = repositories.find(repository => repository.id == id)
+
+  if(!repository){
     return response.status(400).send('Id not found.')
   }
-
-  const repository = repositories.find(repository => repository.id == id)
 
   repository.likes += 1
 
